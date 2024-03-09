2d87dcba6031768d4dec53aa42501090ffbd1f72
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-03-09 13:11:47 +0100

    Add .env.dist file and comment out className in MobileHeader component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -81,11 +81,11 @@ function MobileHeader({
   return (
     <header
       role="banner"
-      className={`${
-        isHome
-          ? 'bg-primary/80  text-contrast  shadow-darkHeader'
-          : 'bg-contrast/80 text-primary'
-      } flex lg:hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`}
+      // className={`${
+      //   isHome
+      //     ? 'bg-primary/80  text-contrast  shadow-darkHeader'
+      //     : 'bg-contrast/80 text-primary'
+      // } flex lg:hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`}
     >
       <div className="flex items-center justify-start w-full gap-4">
         <button
