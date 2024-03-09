795543218632b24d4701a08dcf8a84c45eade238
Thibaut Narivelo <thibaut.randria@gmail.com>
2024-03-09 14:18:02 +0100

    test2

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -130,6 +130,7 @@ function MobileHeader({
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
             w-fit h-fit"
       >
+        hello thibaut
         {isBigHeader && (
           <motion.img
             initial={{opacity: 0}}
